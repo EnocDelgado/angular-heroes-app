b398238dec957e7d098aba90638425ad301f3f2c
Enoc Delgado <105158271+EnocDelgado@users.noreply.github.com>
2023-05-22 20:50:44 -0500

    Section 14

--- src/app/heroes/pages/search-page/search-page.component.ts
+++ src/app/heroes/pages/search-page/search-page.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Hero } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 
 @Component({
   selector: 'app-search-page',
@@ -8,4 +12,29 @@ import { Component } from '@angular/core';
 })
 export class SearchPageComponent {
 
+  public searchInput = new FormControl('');
+  public heroes: Hero[] = [];
+  public selectedHero?: Hero;
+
+  // injecting our service
+  constructor( private heroesServices: HeroesService ) {}
+
+  searchHero() {
+    const value: string = this.searchInput.value || '';
+
+    this.heroesServices.getSuggestions( value )
+      .subscribe(
+        heroes => this.heroes = heroes
+      );
+  }
+
+  onSelectionOption( event: MatAutocompleteSelectedEvent ): void {
+    if ( !event.option.value ) {
+      this.selectedHero = undefined;
+      return;
+    }
+
+    const hero: Hero = event.option.value;
+    this.searchInput.setValue( hero.superhero )
+  }
 }
